389f241c39ba9c225b2034c892592f660400a577
Tejaspatil1175 <tejaspatil1175@gmail.com>
2024-08-13 14:05:44 +0530

    Add navbar, footer, and loading spinner

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa6";
 
 const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <footer>
+        <div>
+          <img src="/logo.png" alt="logo" />
+        </div>
+        <div>
+          <h4>Support</h4>
+          <ul>
+            <li>AT POST JUNE BHAMPUR TAL SHIRPUR</li>
+            <li>tejaspatil@gmail.com</li>
+            <li>+91 8788244416</li>
+          </ul>
+        </div>
 
-export default Footer
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li to={"/"}>
+              <Link>Home</Link>
+            </li>
+            <li to={"/jobs"}>
+              <Link>Jobs</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <ul>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareXTwitter />
+                </span>
+                <span>Twitter (X)</span>
+              </Link>
+            </li>
+           
+            
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">
+        &copy; CopyRight 2024. All Rights Reserved By CodeWithTejas
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { ClipLoader } from "react-spinners";
 
 const Spinner = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section
+        style={{
+          minHeight: "525px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ClipLoader size={150} />
+      </section>
+    </>
+  );
+};
 
-export default Spinner
+export default Spinner;
